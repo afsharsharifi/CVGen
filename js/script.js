28db715e1c9be008d63671a18081ce06dd5623ad
Afshar Sharifi <afsharsharifi2020@gmail.com>
2022-03-07 17:41:54 +0330

    First Resume Template Created

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-// put your scripts here
+//Get the button
+var mybutton = document.getElementById("scroll-top");
+mybutton.style.display = "none";
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
