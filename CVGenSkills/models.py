a28c49ce42a62b2293ab8de67f370db60caffe61
Afshar Sharifi <afsharsharifi2020@gmail.com>
2022-04-23 13:35:26 +0430

    Creating CVGenSkills

---
+++ CVGenSkills/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class UserSkill(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    percentage = models.IntegerField()
